f699311da600448e33950daf1f1a12e0cf7b5ffe
Woong Jun <woong.jun@alleys.co>
2017-11-10 16:33:05 +0900

    fix(mongoose): allow multiple connection to DB considering uses (#15)

--- mongoose.js
+++ mongoose.js
@@ -5,78 +5,80 @@
 'use strict'
 
 
-var mongoose, db, log
+module.exports = function (mongoose) {
+    var db, log
 
+    var init = function (_log) {
+        var nop = function () {}
 
-function connect(conf) {
-    var url = 'mongodb://'
+        log = _log || { info: nop, warning: nop, error: nop }
+    }
 
-    var reconnect = function () {
-        db.open(url, {
-            db: {
-                native_parser: true
-            },
-            server: {
-                socketOptions: {
-                    keepAlve: 1
+    var connect = function (conf) {
+        var url = 'mongodb://'
+
+        var reconnect = function () {
+            db.open(url, {
+                db: {
+                    native_parser: true
                 },
-                auto_reconnect: true
-            },
-            replset: {
-                socketOptions: {
-                    keepAlive: 1
-                }
-            },
-            user: conf.user,
-            pass: conf.password
-        })
-    }
-
-    if (conf.user && conf.password) url += conf.user+':'+conf.password+'@'
-    if (Array.isArray(conf.replSet)) {
-        for (var i = 0; i < conf.replSet.length; i++) {
-            url += ((i > 0)? ',': '')+conf.replSet[i].host+':'+conf.replSet[i].port
+                server: {
+                    socketOptions: {
+                        keepAlve: 1
+                    },
+                    auto_reconnect: true
+                },
+                replset: {
+                    socketOptions: {
+                        keepAlive: 1
+                    }
+                },
+                user: conf.user,
+                pass: conf.password
+            })
         }
-    } else {
-        url += conf.host+':'+conf.port
+
+        if (conf.user && conf.password) url += conf.user+':'+conf.password+'@'
+        if (Array.isArray(conf.replSet)) {
+            for (var i = 0; i < conf.replSet.length; i++) {
+                url += ((i > 0)? ',': '')+conf.replSet[i].host+':'+conf.replSet[i].port
+            }
+        } else {
+            url += conf.host+':'+conf.port
+        }
+        url += '/'+conf.db
+
+        log.info('connecting to '+url)
+
+        db.on('connected', function () {
+            log.info('connected to '+url)
+        })
+        db.on('error', function (err) {
+            log.error(err)
+            db.close()
+        })
+        db.on('disconnected', function () {
+            log.warning('disconnected from '+url+';'+
+                        ' try to reconnect after '+conf.reconnectTime+' sec(s)')
+            setTimeout(reconnect, conf.reconnectTime*1000)
+        })
+
+        reconnect()
     }
-    url += '/'+conf.db
 
-    log.info('connecting to '+url)
+    var close = function () {
+        if (!db) return
 
-    db.on('connected', function () {
-        log.info('connected to '+url)
-    })
-    db.on('error', function (err) {
-        log.error(err)
+        log.info('closing db connection')
+        db.removeAllListeners('disconnected')
         db.close()
-    })
-    db.on('disconnected', function () {
-        log.warning('disconnected from '+url+';'+
-                    ' try to reconnect after '+conf.reconnectTime+' sec(s)')
-        setTimeout(reconnect, conf.reconnectTime*1000)
-    })
+    }
 
-    reconnect()
-}
-
-
-function close() {
-    log.info('closing db connection')
-    db.removeAllListeners('disconnected')
-    db.close()
-}
-
-
-module.exports = function (_mongoose, _log) {
-    var nop = function () {}
-
-    mongoose = _mongoose
-    log = _log || { info: nop, warning: nop, error: nop }
     db = mongoose.createConnection()
 
     return {
         db:      db,
+        init:    init,
         connect: connect,
         close:   close
     }
