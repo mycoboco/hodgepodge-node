dd71b0a31349b7cfdd2f7d87bb35830d74cb61ce
Jun Woong <woong.jun@gmail.com>
2018-09-27 12:03:13 +0900

    feat(db): add mysql connector using sequelize

---
+++ db/mysql.js
@@ -0,0 +1,62 @@
+/*
+ *  connects to or disconnects from MySQL
+ */
+
+'use strict'
+
+const Sequelize = require('sequelize')
+
+
+module.exports = () => {
+    let conn, log
+
+    function init(
+        _log = {
+            info:    () => {},
+            warning: () => {},
+            error:   () => {}
+        }
+    ){
+        log = _log
+    }
+
+    function connect(conf, cb) {
+        let url = 'mysql://'
+
+        if (conf.user && conf.password) url += `${conf.user}:${conf.password}@`
+        url += `${conf.host}:${conf.port}/${conf.db}`
+
+        log.info(`connecting to ${url}`)
+
+        conn = new Sequelize(url, {
+            query: { raw: true }
+        })
+        conn
+            .authenticate()
+            .then(() => {
+                log.info(`connected to ${url}`)
+                conn.sync()
+                cb(null, conn)
+            })
+            .catch(err => {
+                cb(err)
+            })
+    }
+
+    function close(cb = () => {}) {
+       if (!conn) return
+
+       log.info('closing mysql connection')
+       conn
+           .close()
+           .catch(err => log.error(err))
+    }
+
+    return {
+        init,
+        connect,
+        close
+    }
+}
+
+// mysql.js
