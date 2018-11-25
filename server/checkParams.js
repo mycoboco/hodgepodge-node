805f9b229afa6b0635856ae07a092c0cb0e05638
Jun Woong <woong.jun@gmail.com>
2018-11-25 15:39:02 +0900

    fix(server): use util.inspect to show param values

--- server/checkParams.js
+++ server/checkParams.js
@@ -2,11 +2,12 @@
  *  checks request parameters
  */
 
+const { inspect } = require('util')
 
 module.exports = (res, rules) => {
     return !rules.some(r => {
         if (!r[0]) {
-            res.err(r[1], new Error(`invalid ${r[2]}: ${r[3]}`))
+            res.err(r[1], new Error(`invalid ${r[2]}: ${insepct(r[3])}`))
             return true
         }
     })
