9d9cc88c4dbc860478365b1ce1109282ea0c4195
Jun Woong <woong.jun@gmail.com>
2018-12-09 15:21:01 +0900

    fix(server): fix critical typo

--- server/checkParams.js
+++ server/checkParams.js
@@ -9,7 +9,7 @@ const { inspect } = require('util')
 module.exports = (res, rules) => {
     return !rules.some(r => {
         if (!r[0]) {
-            res.err(r[1], new Error(`invalid ${r[2]}: ${insepct(r[3])}`))
+            res.err(r[1], new Error(`invalid ${r[2]}: ${inspect(r[3])}`))
             return true
         }
     })
