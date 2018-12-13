5dcaeb6964a9f2915fbb4a88ea0db680e68c81b5
Jun Woong <woong.jun@gmail.com>
2018-12-13 11:44:07 +0900

    fix(server): change checkParam to use ServerError

--- server/checkParams.js
+++ server/checkParams.js
@@ -4,12 +4,15 @@
 
 const { inspect } = require('util')
 
+const ServerError = require('./ServerError')
+
 
 // [ assertion, status code, variable name, value ]
 module.exports = (res, rules) => {
     return !rules.some(r => {
         if (!r[0]) {
-            res.err(r[1], new Error(`invalid ${r[2]}: ${inspect(r[3])}`))
+            // expects res.err() to accept ServerError
+            res.err(new ServerError(r[1], `invalid ${r[2]}: ${inspect(r[3])}`))
             return true
         }
     })
