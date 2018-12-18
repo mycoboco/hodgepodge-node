38708109e6e5279e2848f2cfda46df4782a21399
Jun Woong <woong.jun@gmail.com>
2018-12-18 14:22:19 +0900

    feat(server): add parameter checks for checkParams()

--- server/checkParams.js
+++ server/checkParams.js
@@ -9,7 +9,12 @@ const ServerError = require('./ServerError')
 
 // [ assertion, status code, variable name, value ]
 module.exports = (res, rules) => {
+    if (typeof res !== 'object' || typeof res.err !== 'function') {
+        throw new Error('response object is missing')
+    }
+
     return !rules.some(r => {
+        if (!Array.isArray(r)) throw new Error('incorrect rule format; did you miss any commas?')
         if (!r[0]) {
             // expects res.err() to accept ServerError
             res.err(new ServerError(r[1], `invalid ${r[2]}: ${inspect(r[3])}`))
