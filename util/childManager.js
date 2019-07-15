08445e21f411beea3f9bfcb8d4d6dc317aabd1c2
Woong Jun <woong.jun@gmail.com>
2019-07-15 19:12:35 +0900

    feat(util): add childManager to manage child processes

---
+++ util/childManager.js
@@ -0,0 +1,44 @@
+/*
+ *  child process manager
+ */
+
+const childs = []
+const debug = false
+
+
+function add(child) {
+    const handler = () => remove(child)
+
+    debug && console.log(`adding ${child.pid}`)
+    childs.push({
+        handle: child,
+        handler
+    })
+    child.once('exit', handler)
+}
+
+
+function remove(child) {
+    const i = childs.findIndex(c => c.handle === child)
+    if (i < 0) return
+    debug && console.log(`removing ${child.pid}`)
+    childs.splice(i, 1)
+}
+
+
+function clean() {
+    childs.forEach(c => {
+        c.handle.removeListener('exit', c.handler)
+        debug && console.log(`killing ${c.handle.pid}`)
+        c.handle.kill()    // may be unsafe or not work
+    })
+}
+
+
+module.exports = {
+    add,
+    remove,
+    clean
+}
+
+// end of childManager.js
--- util/index.js
+++ util/index.js
@@ -1,8 +1,9 @@
 module.exports = {
-    asyncLoop: require('./asyncLoop'),
-    config:    require('./config'),
-    assert:    require('./assert'),
-    safePipe:  require('./safePipe')
+    asyncLoop:    require('./asyncLoop'),
+    config:       require('./config'),
+    assert:       require('./assert'),
+    safePipe:     require('./safePipe'),
+    childManager: require('./childManager')
 }
 
 // end of index.js
