5804b7ab43d6039c39de21aab0a4e9c337f663ec
Woong Jun <woong.jun@gmail.com>
2023-01-15 16:52:47 +0900

    feat(web): rewrite for recent node version

--- web/minjs.js
+++ web/minjs.js
@@ -2,57 +2,66 @@
  *  minifies and servers js
  */
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs/promises');
+const path = require('path');
 
-const minify = require('minify')
+const minify = require('minify');
 
-
-let pub, log
-
+let pub;
+let log;
 
 function serve(
-    _pub = 'public',
-    _log = {
-        info:    () => {},
-        warning: () => {},
-        error:   () => {}
-    }
+  _pub = 'public',
+  _log = {
+    info: () => {},
+    warning: () => {},
+    error: () => {},
+  },
 ) {
-    pub = _pub
-    log = _log
+  pub = _pub;
+  log = _log;
 
-    return (req, res, next) => {
-        const dir = path.dirname(req.url)
-        const name = path.basename(req.url).substring(1)    // foo.js from _foo.js
+  return async (req, res, _next) => {
+    const dir = path.dirname(req.url);
+    const name = path.basename(req.url).substring(1); // foo.js from _foo.js
 
-        minify(path.join(pub, dir, `+${name}`))
-            .then(result => {
-                res.header('Content-type', 'text/javascript')
-                   .send(result)
-                fs.writeFile(path.join(pub, req.url), result, err => {
-                    err && log.error(err)
-                })
-            })
-            .catch(err => {
-                log.error(err)
-                next()
-            })
+    try {
+      const result = await minify(path.join(pub, dir, `+${name}`));
+      res.header('Content-type', 'text/javascript')
+        .send(result);
+      await fs.writeFile(path.join(pub, req.url), result);
+    } catch (err) {
+      err && log.error(err);
     }
+  };
 }
-
 
 function filter(req, res, next) {
-    if (/(?:.*\/|^)\+[^/]+\.js$/.test(req.path)) {    // blocks +foo.js
-        return res.sendStatus(404)
-    }
-    next()
+  if (/(?:.*\/|^)\+[^/]+\.js$/.test(req.path)) { // blocks +foo.js
+    return res.sendStatus(404);
+  }
+  next();
 }
 
-
 module.exports = {
-    serve,
-    filter
+  serve,
+  filter,
+};
+
+// eslint-disable-next-line no-constant-condition
+if (false) {
+  const m = module.exports;
+  const res = {
+    header: (k, v) => {
+      console.log(k, v);
+      return {
+        send: (d) => console.log(d),
+      };
+    },
+  };
+
+  m.serve('test', console)({url: '_foo.js'}, res);
+  m.filter({path: 'test/+foo.js'}, {sendStatus: (c) => console.log(c)});
 }
 
 // end of minjs.js
--- web/sass.js
+++ web/sass.js
@@ -2,58 +2,70 @@
  *  compiles and servers scss
  */
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const sass = require('node-sass')
+const sass = require('node-sass');
 
-
-let pub, log
-
+let pub;
+let log;
 
 function serve(
-    _pub = 'public',
-    _log = {
-        info:    () => {},
-        warning: () => {},
-        error:   () => {}
-    }
+  _pub = 'public',
+  _log = {
+    info: () => {},
+    warning: () => {},
+    error: () => {},
+  },
 ) {
-    pub = _pub
-    log = _log
+  pub = _pub;
+  log = _log;
 
-    return (req, res, next) => {
-        const dir = path.dirname(req.url)
-        const name = path.basename(req.url, path.extname(req.url))
-                         .substring(1)    // foo from _foo.css
+  return (req, res, next) => {
+    const dir = path.dirname(req.url);
+    const name = path.basename(req.url, path.extname(req.url)).substring(1); // foo from _foo.css
 
-        sass.render({
-            file:        path.join(pub, dir, `+${name}.scss`),
-            outputStyle: 'compressed'
-        }, (err, result) => {
-            if (err) {
-                log.error(err)
-                return next()
-            }
-            res.header('Content-type', 'text/css')
-               .send(result.css)
-            fs.writeFile(path.join(pub, req.url), result.css, err => err && log.error(err))
-        })
-    }
+    sass.render({
+      file: path.join(pub, dir, `+${name}.scss`),
+      outputStyle: 'compressed',
+    }, (err, result) => {
+      if (err) {
+        log.error(err);
+        return next();
+      }
+      res.header('Content-type', 'text/css')
+        .send(result.css);
+      fs.writeFile(path.join(pub, req.url), result.css, (err) => err && log.error(err));
+    });
+  };
 }
-
 
 function filter(req, res, next) {
-    if (/(?:.*\/|^)\+[^/]+\.scss$/.test(req.path)) {    // blocks +foo.scss
-        return res.sendStatus(404)
-    }
-    next()
+  if (/(?:.*\/|^)\+[^/]+\.scss$/.test(req.path)) { // blocks +foo.scss
+    return res.sendStatus(404);
+  }
+  next();
 }
 
-
 module.exports = {
-    serve,
-    filter
+  serve,
+  filter,
+};
+
+// eslint-disable-next-line no-constant-condition
+if (false) {
+  const s = module.exports;
+  const res = {
+    header: (k, v) => {
+      console.log(k, v);
+      return {
+        send: (d) => console.log(d.toString('utf8')),
+      };
+    },
+  };
+
+  s.serve('test', console)({url: '_foo.css'}, res);
+  s.filter({path: 'test/+foo.scss'}, {sendStatus: (c) => console.log(c)});
 }
 
 // end of sass.js
