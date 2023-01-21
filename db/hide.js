ec8e742d5c2fd049182f9d2e3255543072b0c212
Woong Jun <woong.jun@gmail.com>
2023-01-21 18:45:03 +0900

    fix(db): support password-only url in hide

--- db/hide.js
+++ db/hide.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = (url = '') => url.replace(
-  /([a-z]+:\/\/)?([^:]+)(:.+)?(@.*)/,
+  /([a-z]+:\/\/)?([^:]*)(:.+)?(@.*)/,
   (_match, p1 = '', p2 = '', p3 = '', p4 = '') => `${p1}${p2}${(p3) ? ':???' : ''}${p4}`,
 );
 
@@ -16,10 +16,10 @@ if (false) {
   console.log(hide('mysql://')); // mysql://
   console.log(hide('mysql://foo1!:bar1!@example.com')); // mysql://foo1!:???@example.com
   console.log(hide('mysql://foo1!@example.com')); // mysql://foo1!@example.com
-  console.log(hide('mysql://:bar1!@example.com')); // invalid url
+  console.log(hide('mysql://:bar1!@example.com')); // mysql://:???@example.com
   console.log(hide('mysql://example.com')); // mysql://example.com
   console.log(hide('foo1!:bar1!@example.com')); // foo1!:???@example.com
-  console.log(hide(':bar1!@example.com')); // invalid url
+  console.log(hide(':bar1!@example.com')); // :???@example.com
   console.log(hide('foo1!@example.com')); // foo1!@example.com
   console.log(hide('@example.com')); // invalid url
   console.log(hide('example.com')); // example.com
