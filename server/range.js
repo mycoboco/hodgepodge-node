a61840e1b4e39d8a002333aed5fa4a9bcff682ea
Woong Jun <woong.jun@gmail.com>
2023-01-25 20:37:23 +0900

    style(server): apply new style to range.js

--- server/range.js
+++ server/range.js
@@ -2,40 +2,37 @@
  *  handles range information from HTTP headers
  */
 
-
 function parse(range, stats) {
-    function parse(r) {
-        r = /bytes=([0-9]+)-([0-9]+)?/.exec(r)
-        if (!r) return null
+  const p = (r) => {
+    r = /bytes=([0-9]+)-([0-9]+)?/.exec(r);
+    if (!r) return null;
 
-        return {
-            s: +r[1],
-            e: +r[2]
-        }
+    return {
+      s: +r[1],
+      e: +r[2],
+    };
+  };
+
+  if (!range) return null;
+
+  const r = p(range);
+  if (r) {
+    if (r.e !== r.e) r.e = stats.size - 1;
+    if (r.s >= stats.size || r.e >= stats.size || r.s > r.e) {
+      return new Error(`invalid range request: ${range}`);
     }
+  }
 
-    if (!range) return null
-
-    const r = parse(range)
-    if (r) {
-        if (r.e !== r.e) r.e = stats.size-1
-        if (r.s >= stats.size || r.e >= stats.size || r.s > r.e) {
-            return new Error(`invalid range request: ${range}`)
-        }
-    }
-
-    return r
+  return r;
 }
-
 
 function header(r, stats) {
-    return `bytes ${r.s}-${r.e}/${stats.size}`
+  return `bytes ${r.s}-${r.e}/${stats.size}`;
 }
-
 
 module.exports = {
-    parse,
-    header
-}
+  parse,
+  header,
+};
 
 // end of range.js
