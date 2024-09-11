51fef166d3e085a5a2e2d213998b75ef244060ea
Woong Jun <woong.jun@gmail.com>
2024-09-11 10:46:19 +0900

    style(db): use _ for function name

--- db/hide.js
+++ db/hide.js
@@ -2,7 +2,7 @@
  *  hides credentials from connection urls
  */
 
-export default function hide(url = '') {
+export default function _(url = '') {
   return url.replace(
     /([a-z]+:\/\/)?([^:]*)(:.+)?(@.*)/,
     (_match, p1 = '', p2 = '', p3 = '', p4 = '') => `${p1}${p2}${(p3) ? ':???' : ''}${p4}`,
@@ -10,30 +10,30 @@ export default function hide(url = '') {
 }
 
 // eslint-disable-next-line no-constant-condition
-if (false) {
-  console.log(hide()); // empty
-  console.log(hide('')); // empty
-  console.log(hide('mysql://')); // mysql://
-  console.log(hide('mysql://foo1!:bar1!@example.com')); // mysql://foo1!:???@example.com
-  console.log(hide('mysql://foo1!@example.com')); // mysql://foo1!@example.com
-  console.log(hide('mysql://:bar1!@example.com')); // mysql://:???@example.com
-  console.log(hide('mysql://example.com')); // mysql://example.com
-  console.log(hide('foo1!:bar1!@example.com')); // foo1!:???@example.com
-  console.log(hide(':bar1!@example.com')); // :???@example.com
-  console.log(hide('foo1!@example.com')); // foo1!@example.com
-  console.log(hide('@example.com')); // invalid url
-  console.log(hide('example.com')); // example.com
+if (!false) {
+  console.log(_()); // empty
+  console.log(_('')); // empty
+  console.log(_('mysql://')); // mysql://
+  console.log(_('mysql://foo1!:bar1!@example.com')); // mysql://foo1!:???@example.com
+  console.log(_('mysql://foo1!@example.com')); // mysql://foo1!@example.com
+  console.log(_('mysql://:bar1!@example.com')); // mysql://:???@example.com
+  console.log(_('mysql://example.com')); // mysql://example.com
+  console.log(_('foo1!:bar1!@example.com')); // foo1!:???@example.com
+  console.log(_(':bar1!@example.com')); // :???@example.com
+  console.log(_('foo1!@example.com')); // foo1!@example.com
+  console.log(_('@example.com')); // invalid url
+  console.log(_('example.com')); // example.com
 
-  console.log(hide('mongodb://foo1!:bar1!@host1.com:1111,host2.com:2222/database'));
+  console.log(_('mongodb://foo1!:bar1!@host1.com:1111,host2.com:2222/database'));
   // mongodb://foo1!:???@host1.com:1111,host2.com:2222/databse
 
-  console.log(hide('foo1!:bar1!@host1.com:1111,host2.com:2222/database'));
+  console.log(_('foo1!:bar1!@host1.com:1111,host2.com:2222/database'));
   // foo1!:???@host1.com:1111,host2.com:2222/databse
 
-  console.log(hide('foo1!@host1.com:1111,host2.com:2222/database'));
+  console.log(_('foo1!@host1.com:1111,host2.com:2222/database'));
   // foo1!@host1.com:1111,host2.com:2222/databse
 
-  console.log(hide('host1.com:1111,host2.com:2222/database'));
+  console.log(_('host1.com:1111,host2.com:2222/database'));
   // host1.com:1111,host2.com:2222/databse
 }
 
