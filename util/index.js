23826bd53942a41d773ab2cf037cc55bfc794bd7
Woong Jun <woong.jun@gmail.com>
2024-10-04 20:03:23 +0900

    fix(util): give name for exported modules

--- util/index.js
+++ util/index.js
@@ -1,7 +1,7 @@
 export {default as config} from './config.js';
-export * from './assert.js';
+export * as assert from './assert.js';
 export {default as safePipe} from './safePipe.js';
-export * from './childManager.js';
+export * as childManager from './childManager.js';
 export {default as recursiveWatch} from './recursiveWatch.js';
 
 // end of index.js
